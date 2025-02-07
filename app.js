f788be348d78c1a9267a57cab9034758200326d0
Nick Monsma <nmonsma@gmail.com>
2025-02-07 16:58:44 -0500

    disable mysql

--- app.js
+++ app.js
@@ -38,24 +38,24 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//MySQL Setup
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'dbuser',
-  password: 's3kreee7',
-  database: 'my_db'
-})
+// //MySQL Setup
+// const mysql = require('mysql')
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'dbuser',
+//   password: 's3kreee7',
+//   database: 'my_db'
+// })
 
-connection.connect()
+// connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
+// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//   if (err) throw err
 
-  console.log('The solution is: ', rows[0].solution)
-})
+//   console.log('The solution is: ', rows[0].solution)
+// })
 
-connection.end()
+// connection.end()
 
 
 module.exports = app;
