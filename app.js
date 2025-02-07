d80e851b14ece060b387b20aa132ce3b010e8145
Nick Monsma <nmonsma@gmail.com>
2025-02-07 16:47:11 -0500

    styling and view

--- app.js
+++ app.js
@@ -38,4 +38,24 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//MySQL Setup
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
+
 module.exports = app;
---
+++ public/javascripts/viewactions.js
@@ -0,0 +1,34 @@
+//Associate form options with classes
+const formAssociations = [
+    {"checkbox": "areas-checkbox",          "cssClass": "area-title"},
+    {"checkbox": "sections-checkbox",       "cssClass": "section-title"},
+    {"checkbox": "policy-numbers-checkbox", "cssClass": "policy-number"},
+    {"checkbox": "audiences-checkbox",      "cssClass": "policy-audience"},
+    {"checkbox": "content-checkbox",        "cssClass": "content"},
+    {"checkbox": "approval-info-checkbox",  "cssClass": "approval"}
+]
+
+function showHide() {
+    //Find the css class associated with the option selected and toggle the hidden class to all elements of that css class.
+
+    let cssClass = '';
+    console.log(this.id);
+
+    for (const item of formAssociations) {
+        if (item.checkbox == this.id) {
+            cssClass = item.cssClass
+        }
+    }
+    const elements = document.getElementsByClassName(cssClass);
+
+    //Rewrite this to overwrite the hidden status based on the the property 'checked' of the checkbox:
+    for (const element of elements) {
+        element.classList.toggle('hidden');
+    }
+
+}
+
+//Attach Event Listeners
+for (const object of document.getElementsByClassName('sidebar-checkbox')) {
+    object.addEventListener('click', showHide); 
+}
