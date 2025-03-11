1d17720e0620b89bf3539eb5d4abfab1bae54914
Nick <nmonsma@gmail.com>
2025-03-11 07:08:29 -0400

    update get routes to include /epcspolicy/

--- public/javascripts/loadactions.js
+++ public/javascripts/loadactions.js
@@ -188,50 +188,6 @@ const loadPolicyTable = async (route)=> {
     
 }
 
-
-
-// Sample Result:
-// [
-//    {
-//         "policy_id": 46,
-//         "policy_number": 2032,
-//         "title": "Admission Procedures",
-//         "section_number": 2000,
-//         "content": "Upon completion of the application, a home visit with the family will be arranged with an EPCS Board of Trustees representative and a member of the administration. The admissions process may include interviewing, testing, evaluating past school records. Enrollment and placement information relevant to making a wise decision is shared with appropriate members of the community. Recommendations may be sought from other community members and/or friends of EPCS. Non-disclosure of essential information could result in the withdrawal of an acceptance. The board member will make an admissions recommendation of the family to the EPCS Board for final approval. All new students are on probation for the first ten weeks.",
-//         "status": "approved",
-//         "entity": "Board",
-//         "date": "2024-10-08 00:00:00",
-//         "handbooks": "F"
-//     },
-//     {
-//         "policy_id": 47,
-//         "policy_number": 2033,
-//         "title": "Admission Requirements for the Parents",
-//         "section_number": 2000,
-//         "content": "At its heart, EPCS is a Christian institution whose goals are the nurturing and instruction of children from Christian families. We teach the Lordship of Jesus Christ in all the curricular subjects and in all our school's activities. The school is seen as an extension of the Christian home, a place where the values and beliefs which the parents teach are further developed and enhanced. Therefore, those families seeking admission to the school must share the school's goals for their child. At least one parent or legal guardian must be an active member of a Christian church and profess faith in Jesus Christ as Lord and Savior. The parents should be willing to cooperate with all the written policies of East Palmyra Christian School. This is most important in the area of discipline and schoolwork standards, as well as active communication with the respective teacher(s) and administration.",
-//         "status": "approved",
-//         "entity": "Board",
-//         "date": "2024-10-08 00:00:00",
-//         "handbooks": "F"
-//     },
-//     {
-//         "policy_id": 48,
-//         "policy_number": 2040,
-//         "title": "Spiritual Qualifications",
-//         "section_number": 2000,
-//         "content": "All candidates for employment must have:\n-Whole-hearted agreement with the East Palmyra Christian School Statement of Faith.\n-From all accounts and appearances, clear evidence of a personal commitment to the Lord Jesus Christ.\n-Not a recent convert (less than two years).\n-An obvious working knowledge of and adherence to the Scriptures, especially in regard to families, marriage, children, and authority found in passages such as Gen. 2:18-25, Matt. 6:31,32, Eph. 6:1-4, Heb. 13:4.",
-//         "status": "approved",
-//         "entity": "Board",
-//         "date": "2024-10-08 00:00:00",
-//         "handbooks": "S"
-//     }
-// ]
-
-//Get Major Dom Element
-
-    
-
-//Main Actions
 //Update policyTable
 loadPolicyTable('all_policies');
 //Iterate through rows
