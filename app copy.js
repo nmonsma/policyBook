329254c0adf5ef4434c6dbf2b0ce404d4a579b9d
Nick <nmonsma@gmail.com>
2026-07-14 12:23:42 -0400

    adding history viewing

---
+++ app copy.js
@@ -0,0 +1,188 @@
+/*Query Constants*/
+
+/*Express and other requires*/
+var path = require('path');
+const POLICY_QUERIES = require('./sqlkeys.js');
+const express = require('express');
+const app = express();
+const credentials = require('./credentials.js');
+
+/* Middleware*/
+//Configure the app to use Express:
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+console.log('App started at:', new Date().toLocaleString());
+
+// //MySQL Setup
+// const mysql = require('mysql2');
+// const dbConnection = mysql.createConnection({
+//   host: credentials.host,
+//   user: credentials.user,
+//   password: credentials.password,
+//   database: credentials.database
+// });
+// dbConnection.connect((err) => {
+//     if (err) throw err;
+//     console.log('connected to database');
+//   });
+  
+  //Create Routes
+  app.get('/epcspolicy/all_policies', (req, res) => {
+    delete require.cache[require.resolve('./sqlkeys.js')];
+    const POLICY_QUERIES = require('./sqlkeys.js');
+    
+    const sql = POLICY_QUERIES[req.query.filter];
+    if (!sql) return res.status(400).json({ error: 'Invalid query key' });
+    dbConnection.query(sql, (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+  
+  app.get('/epcspolicy/employee_policies', (req, res) => {
+    dbConnection.query('SELECT * FROM policies WHERE handbook_e = 1 ORDER BY policy_number', (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+  
+  app.get('/epcspolicy/family_policies', (req, res) => {
+    dbConnection.query('SELECT * FROM policies WHERE handbook_f = 1 ORDER BY policy_number', (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+  
+  app.get('/epcspolicy/extracurricular_policies', (req, res) => {
+    dbConnection.query('SELECT * FROM policies WHERE handbook_x = 1 ORDER BY policy_number', (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+  
+  app.get('/epcspolicy/headings', (req, res) => {
+    dbConnection.query('SELECT * FROM headings ORDER BY heading_id', (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+  app.get('/epcspolicy/board_pending', (req, res) => {
+    dbConnection.query("SELECT * FROM policies WHERE LOWER(entity) LIKE '%board%' AND NOT status = 'approved' ORDER BY policy_number", (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+  app.get('/epcspolicy/admin_pending', (req, res) => {
+    dbConnection.query("SELECT * FROM policies WHERE LOWER(entity) LIKE '%admin%' AND NOT status = 'approved' ORDER BY policy_number", (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+  app.get('/epcspolicy/approved', (req, res) => {
+    dbConnection.query("SELECT * FROM policies WHERE status = 'approved' ORDER BY policy_number", (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+  app.get('/epcspolicy/amended', (req, res) => {
+    dbConnection.query("SELECT * FROM policies WHERE status = 'amended' ORDER BY policy_number", (err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+
+
+// //Create Alternate Routes
+//   //Create Routes
+//   app.get('/all_policies', (req, res) => {
+//     delete require.cache[require.resolve('./sqlkeys.js')];
+//     const POLICY_QUERIES = require('./sqlkeys.js');
+    
+//     const sql = POLICY_QUERIES[req.query.filter];
+//     if (!sql) return res.status(400).json({ error: 'Invalid query key' });
+//     dbConnection.query(sql, (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+  
+//   app.get('/employee_policies', (req, res) => {
+//     dbConnection.query('SELECT * FROM policies WHERE handbook_e = 1 ORDER BY policy_number', (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+  
+//   app.get('/family_policies', (req, res) => {
+//     dbConnection.query('SELECT * FROM policies WHERE handbook_f = 1 ORDER BY policy_number', (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+  
+//   app.get('/extracurricular_policies', (req, res) => {
+//     dbConnection.query('SELECT * FROM policies WHERE handbook_x = 1 ORDER BY policy_number', (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+  
+//   app.get('/headings', (req, res) => {
+//     dbConnection.query('SELECT * FROM headings ORDER BY heading_id', (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+
+//   app.get('/board_pending', (req, res) => {
+//     dbConnection.query("SELECT * FROM policies WHERE LOWER(entity) LIKE '%board%' AND NOT status = 'approved' ORDER BY policy_number", (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+
+//   app.get('/admin_pending', (req, res) => {
+//     dbConnection.query("SELECT * FROM policies WHERE LOWER(entity) LIKE '%admin%' AND NOT status = 'approved' ORDER BY policy_number", (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+
+//   app.get('/approved', (req, res) => {
+//     dbConnection.query("SELECT * FROM policies WHERE status = 'approved' ORDER BY policy_number", (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+
+//   app.get('/amended', (req, res) => {
+//     dbConnection.query("SELECT * FROM policies WHERE status = 'amended' ORDER BY policy_number", (err, result) => {
+//       if (err) throw err;
+//       res.json(result);
+//     });
+//   });
+
+
+/*Spin up the Server for online use*/
+// app.use('/epcspolicy', express.static(path.join(__dirname, 'public')));
+// app.listen(process.env.PORT, function () {
+//     console.log('listening')
+//     console.log('Routes registered:', app._router.stack.filter(r => r.route).map(r => r.route.path));
+// })
+
+/*Spin up the Server for local use*/
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.listen(3000, function () {
+    console.log('listening')
+    console.log('Routes registered:', app._router.stack.filter(r => r.route).map(r => r.route.path));
+})
+
+
+
+
